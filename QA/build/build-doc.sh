3d64b4d4bbb73d06e00795d50b95c100abe16539
megaplanet <megaplanet.github@megaplanet.org>
2013-04-03 22:55:29 +0200

    added indexes to build-doc

--- QA/build/build-doc.sh
+++ QA/build/build-doc.sh
@@ -7,23 +7,63 @@ OUTFILE=${OUTDIR?}/README.md
 
 echo `ls $SRCFILESPATTERN | wc -l` source files 
 
-awk '
+gawk '
+    # RULE_TO_PACKAGE: RULENAME -> PACKAGENAME
+    #   
+    # PACKAGES:  PACKAGENAME
+
+
     /^\$.*:$/   {
-      print ; 
-      print "-----" ; 
+      gsub(/^\$/,"") ; gsub(/:$/,"") ;
+      RULENAME=$0 ;
+      RULE_TO_PACKAGE[RULENAME] = PACKAGENAME
+      
+      print "" ;
+      print RULENAME;      
+      print "-------------------" ;
+      print "package " PACKAGENAME ;      
       next
     } 
     
     /^#=[^=]/   {
-      gsub(/#=/,"") ; 
-      print; 
-      print "=====" ; 
+      gsub(/#=+ */,"") ;
+      PACKAGENAME = $0 ;
+      PACKAGES[PACKAGENAME] = PACKAGENAME ;
+      
+      print "" ;
+      print "" ;      
+      print PACKAGENAME; 
+      print "====================================================" ; 
       next 
     } 
     
     /^[^#]/   {
       print $0 "  "
-    }' \
+    }
+    
+    function nameToUrl(name) {
+      return "[" name "](#" tolower(name) ")" ;
+    }
+    
+    END {
+      NBPACKAGES = asorti(PACKAGES, PACKAGENAMES)
+      NBRULES    = asorti(RULE_TO_PACKAGE,    RULENAMES)
+
+
+      print "RULE PACKAGES (" NBPACKAGES ")" ;
+      print "=================" ;
+      for (i = 1; i <= NBPACKAGES; i++) {
+        print "* " nameToUrl(PACKAGENAMES[i]) ; 
+      }
+      
+      print "RULES (" NBRULES ")" ;
+      print "================" ;
+      for (i = 1; i <= NBRULES; i++) {
+        print "* " nameToUrl(RULENAMES[i]) " from " nameToUrl(RULE_TO_PACKAGE[RULENAMES[i]]); 
+      }
+    }
+    
+    ' \
   ${SRCFILESPATTERN} \
   > ${OUTFILE?}
   
