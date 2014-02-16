b2d6ca6f45ebb7a500fb10e4d3d73beeccb9eacc
megaplanet <megaplanet.github@megaplanet.org>
2014-02-17 00:51:18 +0100

    modelioboot

---
+++ QA/build/gen-doc-defaults-for-dir.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+DIR=$1
+EXT_SRC=.xfdf
+EXT_TGT=-dt.html
+THISDIR=`dirname $0`
+
+OUTPUTDIR=$DIR/html
+mkdir -p $OUTPUTDIR
+
+for INPUT in `ls ${DIR}/*$EXT_SRC`
+do
+  CORE=`basename $INPUT $EXT_SRC`
+  OUTPUT=$OUTPUTDIR/$CORE$EXT_TGT
+  echo -n "Processing $CORE ... "
+  cat $INPUT | $THISDIR/gen-doc-defaults.sh >$OUTPUT
+  echo "done"  
+done
